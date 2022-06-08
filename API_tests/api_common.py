23b604d12b048d63442b58854d929ec2c83d930b
sukhanovalexander <31632899+sukhanovalexander@users.noreply.github.com>
2022-06-08 09:23:18 +0300

    preparing api test files

---
+++ API_tests/api_common.py
@@ -0,0 +1,8 @@
+
+import requests
+
+url = 'https://api.github.com/some/endpoint'
+payload = {'some': 'data'}
+headers = {'content-type': 'application/json'}
+
+r = requests.post(url, data=json.dumps(payload), headers=headers)
